fb19efde7e0722693977c6dcdb2b58b73ed59843
Tarun Uppal <tarunpreetuppal2007@gmail.com>
2023-09-28 18:46:02 +0530

    Checking

--- main.py
+++ main.py
@@ -2,16 +2,18 @@
 import tally_functions as tf
 def main():
     tf.open_tally()
-    tf.open_firm("sukh")
-    tf.open_purchase()
-    print("Purchase OPEN")
-    name = "Sunda"
-    date = "20-9-2023"
-    invoice_no = "SMUM1234"
-    part = ["WPA",1, 1200, 10]
-    data = [part, part]
-    tf.put_head(name, date, invoice_no)
-    # tf.put_data(data)
-    # tf.put_closing()
+    activated = tf.is_activated()
+    if activated:
+        tf.open_firm("Uppa")
+        tf.open_purchase()
+        print("Purchase OPEN")
+        name = "SUND"
+        date = "20-9-2023"
+        invoice_no = "SMUM1234"
+        part = ["WPA",1, 1200, 10]
+        data = [part, part]
+        tf.put_head(name, date, invoice_no)
+        tf.put_data(data)
+        # tf.put_closing()
 
 main()
--- tally_functions.py
+++ tally_functions.py
@@ -1,6 +1,12 @@
 import pyautogui as pg
 import time
 
+def is_activated():
+    if pg.locateOnScreen('images/is_activated.png') == None:
+        return None
+    else:
+        return True
+
 """
 Opens Tally and initializes it
 """
@@ -35,47 +41,73 @@ def open_purchase():
         pg.press("v")
         time.sleep(2)
         pg.press("f9")
-        pg.press("enter")
-        while pg.locateOnScreen('images/purchase.png') != None:
-            time.sleep(0.1)
-    
+        while pg.locateOnScreen('images/purchase.png') == None:
+            pg.press("enter")
+        pg.press("backspace")
+    else:
+        return
+
+""" 
+Puts in the invoice no, date, Party name and Purchase Ledger 
+"""
 def put_head(name, date, invoice_no):
     pg.press("f2")
     pg.write(date)
     pg.press("enter")
     pg.write(invoice_no)
-    pg.press("enter")
-    pg.press("enter")
+    while (pg.locateOnScreen('images/party_name.png') == None):
+        pg.press("enter")
     pg.write(name)
     pg.press("down")
-    for i in range(17):
+    pg.press("enter")    
+    while(pg.locateOnScreen('images/purchase.png') == None):
         pg.press("enter")
+    pg.write("GST TAX")
+    pg.press("down")
+    pg.press("enter")
+    while(pg.locateOnScreen('images/purchase.png') == None):
+        pg.press("enter")
+    
         
 def put_data(data):
     for data1 in data:
+        print(str(data1))
         part = str(data1[0])
         quantity = str(data1[1])
         price = str(data1[2])
         disc = str(data1[3])
         pg.write(part)
         pg.press("down")
-        pg.press("down")
         pg.press("enter")
+        while(pg.locateOnScreen('images/purchase.png') == None):
+            pg.press("enter")
+        pg.write(quantity)
+        pg.press("enter")
+        while(pg.locateOnScreen('images/purchase.png') == None):
+            pg.press("enter")
         pg.write(price)
         pg.press("enter")
+        pg.press("enter")
         pg.write(disc)
+        while(pg.locateOnScreen('images/new_product.png') == None):
+            pg.press("enter")
+        
+    # Error before this 
+    while(pg.locateOnScreen('images/end_of_list.png') == None):
         pg.press("enter")
-    for i in range(6):
-        pg.press("enter")
-        print("done")
-    time.sleep(2)
+        print("WOSSSSSSS")
+    # time.sleep(2)
+    pg.press("backspace")
+    
     
 def put_closing():
     pg.write("cgs")
     pg.press("down")
-    for i in range(3):
+    while(pg.locateOnScreen('images/new_product.png') == None):
         pg.press("enter")
     pg.write("sgs")
-    pg.press("down")
-    for i in range(30):
+    while(pg.locateOnScreen('images/new_product.png') == None):
+        pg.press("enter")
+    pg.write("round")
+    while(pg.locateOnScreen('images/end_of_bill.png') == None):
         pg.press("enter")
